b887108969235115080c79892d0509f2a610641e
amir khazama <amirkhazama1139@gmail.com>
2023-05-12 23:59:08 +0330

    serving code listening on port 9000 added

--- auth/main.go
+++ auth/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+
+	pb "github.com/Morning1139Angel/web-hw1/auth/grpc"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("hello from Auth!")
+	// Create a new gRPC server
+	server := grpc.NewServer()
+
+	// Register implementation of the service
+	authService := &authServer{}
+	pb.RegisterAuthServiceServer(server, authService)
+
+	// Create a TCP listener
+	listener, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	// Start serving requests
+	if err := server.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
