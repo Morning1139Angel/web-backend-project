a1b9f5bf04e4be213b6d6ab5e4dcc54b71893577
amir khazama <amirkhazama1139@gmail.com>
2023-06-08 20:47:01 +0330

    gateway done for pq request

--- gateway/main.go
+++ gateway/main.go
@@ -1,44 +1,84 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"context"
+	"log"
 	"net/http"
+	"os"
+
+	pb "github.com/Morning1139Angel/web-hw1/gateway/grpc"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
-func main() {
-	// Define the server configuration
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: http.HandlerFunc(handler),
-	}
+var authServerHost = os.Getenv("AUTH_SERVER_HOST")
+var authServerPort = os.Getenv("AUTH_SERVER_PORT")
+var authClient pb.AuthServiceClient
 
-	// Start the server
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("Error starting server:", err)
-	}
+type PQRequest struct {
+	MessageId uint64 `json:"messageId" binding:"required"` //TODO:add custom odd validator
+	Nonce     string `json:"nonce" binding:"required,len=20"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	// Print the request method (GET, POST, etc.)
-	fmt.Println("Method:", r.Method)
-
-	// Print the request URL
-	fmt.Println("URL:", r.URL)
-
-	// Print the request headers
-	for name, values := range r.Header {
-		for _, value := range values {
-			fmt.Printf("%s: %s\n", name, value)
-		}
-	}
-
-	// Read the request body and print its content
-	body, err := io.ReadAll(r.Body)
+func main() {
+	// Create a gRPC connection to the server
+	targetPath := authServerHost + ":" + authServerPort
+	conn, err := grpc.Dial(targetPath, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		fmt.Println("Error reading body:", err)
+		log.Fatalf("Failed to connect to gRPC server: %v", err)
 	}
-	defer r.Body.Close() // Close the request body when we're done
-	fmt.Println("Body:", string(body))
+	defer conn.Close()
+
+	// Create a gRPC client using the connection
+	authClient = pb.NewAuthServiceClient(conn)
+
+	//start the gin server
+	engine := gin.New()
+	engine.POST("/auth/pq", PQhandler) //TODO: add DH handler
+	engine.Run(":8080")
+}
+
+func PQhandler(c *gin.Context) {
+	//do field checks on the request body
+	body, err := fieldCheckPQBody(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	//send request to server
+	request := pb.PQRequest{MessageId: body.MessageId, Nonce: body.Nonce}
+	pqResponse, err := authClient.PqReq(context.Background(), &request)
+
+	//send the gRPC response back to the client
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, pqResponse)
+
+}
+
+func fieldCheckPQBody(c *gin.Context) (PQRequest, error) {
+	body := PQRequest{}
+	//do the checks specified by the struct tags of PQRequest
+	if err := c.ShouldBindJSON(&body); err != nil {
+		return PQRequest{}, err
+	}
+
+	//check message Id being odd
+	if err := checkMessageIdOddness(body.MessageId); err != nil {
+		return PQRequest{}, err
+	}
+	return body, nil
+}
+
+func checkMessageIdOddness(messageId uint64) error {
+	if messageId%2 != 1 {
+		return status.Errorf(codes.InvalidArgument, "messageId must be an odd number")
+	}
+	return nil
 }
