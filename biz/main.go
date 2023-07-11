b43001c1421e808845706a54b528c75d19cf7572
amir khazama <amirkhazama1139@gmail.com>
2023-07-11 18:24:03 +0330

    refactored postgres settings

--- biz/main.go
+++ biz/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+
 	"github.com/Morning1139Angel/web-hw1/biz/myPkgName"
 	"google.golang.org/grpc"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
-	"net"
 )
+
+var dsn = map[string]string{
+	"host":     os.Getenv("POSTGRES_HOST"),
+	"user":     os.Getenv("POSTGRES_USER"),
+	"password": os.Getenv("POSTGRES_PASSWORD"),
+	"dbname":   os.Getenv("POSTGRES_DBNAME"),
+	"port":     os.Getenv("POSTGRES_PORT"),
+	"sslmode":  os.Getenv("POSTGRES_SSLMODE"),
+	"TimeZone": os.Getenv("POSTGRES_TIMEZONE"),
+}
 
 func main() {
 	//TODO: change this and create database first
-	dsn := "host=localhost user=postgres password=test dbname=users port=5432 sslmode=disable TimeZone=Asia/Tehran"
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err := gorm.Open(postgres.Open(mapToString(dsn)), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
@@ -31,4 +44,12 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
+}
+
+func mapToString(m map[string]string) string {
+	var parts []string
+	for key, value := range m {
+		parts = append(parts, fmt.Sprintf("%s=%s", key, value))
+	}
+	return strings.Join(parts, " ")
 }
